55c635d0fc92a3dda66f9c93b2c6accbc332d566
azeus <azmath.gr@gmail.com>
2024-11-17 21:25:33 -0800

    factorial

---
+++ combination.py
@@ -0,0 +1,11 @@
+
+def factorial(x):
+    fact=1
+    for i in range(1,x+1):
+        fact=fact*i
+    return fact
+def main(n,r):
+    diff=n-r
+    comb=factorial(n)/ (factorial(diff)*factorial(r))
+    print(comb)
+
